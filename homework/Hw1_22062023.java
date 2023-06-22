96bf9f5d046d051d0d0a207e7ea35df06fec285b
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2023-06-22 17:15:30 +0200

    Add files via upload

---
+++ homework/Hw1_22062023.java
@@ -0,0 +1,13 @@
+package org.telran.homework;
+
+public class Hw1_22062023 {
+    public static void main(String[] args) {
+        char Character = 'G';
+        int Integer = 89;
+        byte Byte = 4;
+        short Short = 56;
+        float Float = 4.7333436F;
+        double Double = 4.355453532;
+        long Long = 12121;
+    }
+}
