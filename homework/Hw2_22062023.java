3e189a183e1d30afbbb00214ec55410e5030d038
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2023-06-23 10:08:41 +0200

    Delete Hw2_22062023.java

--- homework/Hw2_22062023.java
+++
@@ -1,9 +0,0 @@
-package org.telran.homework;
-
-public class Hw2_22062023 {
-    public static void main(String[] args) {
-        int num = 345;
-        System.out.println(num);
-        System.out.println((byte)num);
-    }
-}
