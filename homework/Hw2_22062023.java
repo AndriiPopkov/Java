d6224e589c56b9ad9f933e1e0c34a855d4aadf45
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2023-06-26 10:33:34 +0200

    Add files via upload

---
+++ homework/Hw2_22062023.java
@@ -0,0 +1,9 @@
+package org.telran.homework;
+
+public class Hw2_22062023 {
+    public static void main(String[] args) {
+        int num = 345;
+        System.out.println(num);
+        System.out.println((byte)num);
+    }
+}
