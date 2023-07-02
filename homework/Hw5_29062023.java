861b84734ceeea58f47872fd95e89674f01bc47a
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2023-07-02 22:12:43 +0200

    Add files via upload

---
+++ homework/Hw5_29062023.java
@@ -0,0 +1,17 @@
+package org.telran.homework;
+
+import java.util.Scanner;
+
+public class Hw5_29062023 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String a = scn.nextLine();
+        String b = scn.nextLine();
+        int part1 = a.length()/2;
+        int part2 = b.length()/2;
+        String result = a.substring(0,part1) + b.substring(part2);
+        System.out.println(result);
+        System.out.println(a.substring(2));
+    }
+}
+
